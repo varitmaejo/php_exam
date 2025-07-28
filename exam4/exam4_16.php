bfe88d5f12f513507f30ff475b501bf747ee0d39
vk <varit.k@rmutsb.ac.th>
2025-07-28 18:20:33 +0700

    Initial commit

--- exam4/exam4_16.php
+++ exam4/exam4_16.php
@@ -1,28 +1,17 @@
 <?php
 //แสดงเมนูโปรแกรม
-$choice = 0;
-
-do {
-    echo "<br>=== เมนูหลัก ===<br>";
+$choice    = 0;
+$iteration = 0;
+$choices   = [1, 2, 3];
+// รายการตัวเลือกที่จะจำลอง
+do {echo "<br>=== เมนูหลัก (รอบที่ " . ($iteration + 1) . ") ===<br>";
     echo "1. ดูข้อมูลส่วนตัว<br>";
     echo "2. แก้ไขข้อมูล<br>";
     echo "3. ออกจากระบบ<br>";
     echo "กรุณาเลือก (1-3): ";
-    
-    $choice = 2; // จำลองการเลือก
-    
-    switch ($choice) {
-        case 1:
-            echo "แสดงข้อมูลส่วนตัว<br>";
-            break;
-        case 2:
-            echo "แก้ไขข้อมูลเรียบร้อย<br>";
-            break;	
-        case 3:
-            echo "ออกจากระบบแล้ว<br>";
-            break;
-        default:
-            echo "กรุณาเลือก 1-3 เท่านั้น<br>";
-    }
-} while ($choice != 3);
-?>
+// จำลองการเลือกแบบหมุนเวียน
+    $choice = $choices[$iteration % count($choices)];
+    echo $choice . "<br>";switch ($choice) {case 1:echo "แสดงข้อมูลส่วนตัว<br>";
+            break;case 2:echo "แก้ไขข้อมูลเรียบร้อย<br>";
+            break;case 3:echo "ออกจากระบบแล้ว<br>";
+            break;default: echo "กรุณาเลือก 1-3 เท่านั้น<br>";}$iteration++;} while ($choice != 3);
