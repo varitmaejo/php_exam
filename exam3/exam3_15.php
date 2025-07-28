1d513da89f40dac06b8b701bdb1f5539423eb095
vk <varit.k@rmutsb.ac.th>
2025-07-28 18:06:50 +0700

    Initial commit

--- exam3/exam3_15.php
+++ exam3/exam3_15.php
@@ -2,7 +2,8 @@
 //print - แสดงผลแบบคืนค่า
 // print คืนค่า 1 เสมอ 
 $result = print "Hello World!";
-echo "<br>ค่าที่คืน: $result<br>";
-// สามารถใช้ใน expression 
-$output = print ("ข้อความนี้จะแสดงและคืนค่า 1") + 5;
-echo "<br>ผลลัพธ์: $output<br>";
+echo "<br>ค่าที่คืน: $result<br>"; // แสดงผลก่อน 
+print("ข้อความนี้จะแสดงและคืนค่า 1");
+echo "<br>"; // จากนั้นคำนวณแยกต่างหาก 
+$output = 1 + 5; // เพราะ print คืนค่า 1 เสมอ 
+echo "ผลลัพธ์: $output<br>";
